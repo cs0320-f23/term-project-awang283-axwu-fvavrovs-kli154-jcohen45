244d480902e951451a4b0d8d613636124afc1185
Jaclyn Cohen :) <105396093+jackieac04@users.noreply.github.com>
2023-11-30 15:42:31 -0500

    feat: working on searchbar

---
+++ client/src/App.tsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
+import Happenings from "./components/Happenings";
+import Archive from "./components/Archive";
+import About from "./components/About";
+import "./styles/App.css";
+import { Button } from "./components/Button";
+
+// import { Button } from "@mui/material";
+export default function App() {
+  const [loggedIn] = useState<boolean>(false);
+  return (
+    <>
+      <article>
+        <header>
+          <nav>
+            <div className="left-links">
+              <Link to="/home" id="logo">
+                P@B
+              </Link>
+              <Link to="/happenings">Happenings</Link>
+              <Link to="/archive">Archive</Link>
+              <Link to="/about">About</Link>
+            </div>
+            <div className="right-links">
+              {loggedIn ? (
+                <>
+                  <button>+</button>
+                  <button>User Profile</button>
+                </>
+              ) : (
+                <>
+                  {" "}
+                  <Button text={"Login"} />
+                  <Button text={"Sign up"} />
+                </>
+              )}
+            </div>
+          </nav>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route path="/happenings" element={<Happenings />} />
+            <Route path="/archive" element={<Archive />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </main>
+      </article>
+    </>
+  );
+}
--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,10 +1,17 @@
 import React from "react";
+import "../styles/Home.css";
 
 export default function Home() {
   return (
-    <div>
-    
-      <main></main>
-    </div>
+    <main className="posters">
+      <label className="label">
+        <div className="text-wrapper">Posters @ Brown</div>
+      </label>
+      <div className="home-content">
+        <input className="input" placeholder="Search" type="text" />
+        {/* map each poster to an img w/in a div  */}
+        <div>Right now nothing is in here.</div>
+      </div>
+    </main>
   );
 }
