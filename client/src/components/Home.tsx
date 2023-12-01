7ed0a15a0d38f6c4e2e0a929709fae35b332a497
Jaclyn Cohen :) <105396093+jackieac04@users.noreply.github.com>
2023-11-30 20:35:01 -0500

    feat: working on tags

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -9,8 +9,15 @@ export default function Home() {
       </label>
       <div className="home-content">
         <input className="input" placeholder="Search" type="text" />
+        <div className="tags">
+          <span className="text">Suggested tags:</span>{" "}
+          <div className="tag-holder">
+            <div className="magenta-tag">free food</div>
+            <div className="green-tag">Party</div>
+          </div>
+        </div>
+        <div className="happenings-label">Happenings Today</div>
         {/* map each poster to an img w/in a div  */}
-        <div>Right now nothing is in here.</div>
       </div>
     </main>
   );
