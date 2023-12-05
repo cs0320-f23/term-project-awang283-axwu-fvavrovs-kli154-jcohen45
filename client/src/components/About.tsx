6337460d0ec481c4800b5ad3c4587fe107ac9dab
Jaclyn Cohen :) <105396093+jackieac04@users.noreply.github.com>
2023-12-05 10:44:13 -0500

    about stuff:

--- client/src/components/About.tsx
+++ client/src/components/About.tsx
@@ -4,12 +4,21 @@ import "../styles/About.css";
 export default function About() {
   return (
     <>
-      <main>
-        <h1> About Us</h1>
-        <p>Insert some text about why we made this</p>
-        <p>
-          Insert some text about each of us 5 and maybe a lil pic or drawing?
-        </p>
+      <main style={{ display: "flex" }}>
+        <div
+          className="content"
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+          }}
+        >
+          <h1> About Us</h1>
+          <p>Insert some text about why we made this</p>
+          <p>
+            Insert some text about each of us 5 and maybe a lil pic or drawing?
+          </p>
+        </div>
       </main>
     </>
   );
