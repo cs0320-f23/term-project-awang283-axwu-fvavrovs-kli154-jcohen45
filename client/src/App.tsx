f7a21a24b33716d4ca977d7b3519373b0de073e3
Jaclyn Cohen :) <105396093+jackieac04@users.noreply.github.com>
2023-12-20 10:50:04 -0500

    try

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -13,13 +13,7 @@ import {
   useGoogleLogin,
 } from "@react-oauth/google";
 import axios from "axios";
-
-interface IUser {
-  id?: string;
-  name?: string;
-  email?: string;
-  posters?: string[];
-}
+import { createUser } from "./functions/fetch";
 
 export default function App() {
   const [modalOpen, setModalOpen] = useState<string>("");
@@ -46,73 +40,39 @@ export default function App() {
         .then(async (res) => {
           if (res.data.hd == "brown.edu" || res.data.hd == "risd.edu") {
             setProfile(res.data);
-          } else if (res.data.hd != "brown.edu" || res.data.hd != "risd.edu") {
-            window.alert("Please use a valid Brown or RISD email");
           } else {
+            window.alert("Please use a valid Brown or RISD email");
             setProfile(null);
-          }
-          //check if user exists in database w get user by id
-          console.log(profile);
-          if (profile) {
-            try {
-              const userID = profile.id;
-              const foundUser = await fetch(
-                "http://localhost:8080/users/" + userID
-              );
-              if (foundUser.ok) {
-                const userValid = await foundUser.json();
-
-                if (userValid.data) {
-                  setProfile(profile);
-                }
-                //if not, call create user
-                else {
-                  try {
-                    const user: IUser = {
-                      id: profile.id,
-                      name: profile.name,
-                      email: profile.email,
-                    };
-
-                    console.log(profile);
-                    //add to database
-                    const config = {
-                      headers: {
-                        "Content-Type": "application/json",
-                      },
-                    };
-                    const url = "http://localhost:8080/users/create/";
-                    // const formData = new FormData();
-
-                    // formData.append("id", user.id!);
-                    // formData.append("name", user.name!);
-                    // formData.append("email", user.email!);
-
-                    // console.log(
-                    //   JSON.stringify(Array.from(formData)) + " formdata"
-                    // );
-                    const res = await axios.post(url, user, config);
-                    console.log(res);
-                    return Promise.resolve(res.data.data);
-                  } catch (error) {
-                    if (axios.isAxiosError(error) && error.response) {
-                      return Promise.resolve(
-                        `Error in fetch: ${error.response.data.message}`
-                      );
-                    } else {
-                      return Promise.resolve(
-                        "Error in fetch: Network error or other issue"
-                      );
-                    }
-                  }
-                }
-              }
-            } catch (e) {
-              console.log("error fetching user" + e);
-            }
           }
         })
         .catch((err) => console.log(err));
+      //check if user exists in database w get user by id
+      console.log(profile);
+      const findUser = async () => {
+        if (profile) {
+          try {
+            const userID = profile.id;
+            const foundUser = await fetch(
+              "http://localhost:8080/users/" + userID
+            );
+            if (foundUser.ok) {
+              const userValid = await foundUser.json();
+
+              if (userValid.data) {
+                setProfile(profile);
+              } else {
+                //if not, call create user
+                return await createUser(profile);
+              }
+            }
+          } catch (e) {
+            console.log("error fetching user" + e);
+          }
+        }
+      };
+      findUser().then((p) => {
+        return p;
+      });
     }
   }, [user]);
 
--- client/src/functions/fetch.ts
+++ client/src/functions/fetch.ts
@@ -1,5 +1,14 @@
 //for functions where we use the same backend call multiple times
 
+import axios from "axios";
+
+interface IUser {
+  id?: string;
+  name?: string;
+  email?: string;
+  posters?: string[];
+}
+
 export async function fetchTags() {
   try {
     const response = await fetch("http://localhost:8080/posters/alltags");
@@ -13,3 +22,35 @@ export async function fetchTags() {
     console.error("Error fetching tags:", error);
   }
 } //notes : liking posts, making it an app, giving users recs by interests
+
+export async function createUser(profile) {
+  try {
+    const user: IUser = {
+      id: profile.id,
+      name: profile.name,
+      email: profile.email,
+    };
+
+    console.log(profile);
+    //add to database
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+      },
+      withCredentials: true,
+    };
+    const url = "http://localhost:8080/users/create/";
+
+    const res = await axios.post(url, user, config);
+    console.log(res);
+    return Promise.resolve(res.data.data);
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response) {
+      return Promise.resolve(`Error in fetch: ${error.response.data.message}`);
+    } else {
+      return Promise.resolve("Error in fetch: Network error or other issue");
+    }
+  }
+}
