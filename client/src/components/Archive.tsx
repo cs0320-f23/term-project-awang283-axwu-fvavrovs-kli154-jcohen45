ca4db56d1cb5227fe4a68a35598280ae3cdfd2f9
Anna Wang <anna_wang@brown.edu>
2024-01-10 23:25:54 +0800

    feat: archive now maps items to their years

--- client/src/components/Archive.tsx
+++ client/src/components/Archive.tsx
@@ -1,4 +1,3 @@
-// import React from "react";
 import "../styles/Archive.css";
 import { TriangleUpIcon } from "@chakra-ui/icons";
 import { IPoster, ImageCard } from "./Happenings";
@@ -6,17 +5,45 @@ import { Box, IconButton } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { fetchTags } from "../functions/fetch";
 import axios from "axios";
-import Masonry from "react-responsive-masonry";
+import Masonry from "masonry-layout";
+import imagesLoaded from "imagesloaded";
+import React from "react";
 
 export default function Archive() {
   const [searchResults, setSearchResults] = useState<IPoster[]>([]);
   const [allTags, setAllTags] = useState<string[]>([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [posters, setPosters] = useState<{ [key: string]: IPoster[] }>({});
+
+  useEffect(() => {
+    for (const year of Object.keys(posters)) {
+      imagesLoaded(`#grid-${year}`, function () {
+        new Masonry(`#grid-${year}`, {
+          columnWidth: 34,
+          itemSelector: ".image-card",
+          gutter: 23,
+        });
+      });
+    }
+  }, [searchResults]);
+
+  function mapPosters(archivePosters: IPoster[]) {
+    const mappedPosters: { [key: string]: IPoster[] } = {};
+    for (const poster of archivePosters) {
+      //keys have to be strings, so number date is converted to string automatically
+      if (!mappedPosters[poster.startDate[0]]) {
+        mappedPosters[poster.startDate[0]] = [];
+      }
+      mappedPosters[poster.startDate[0]].push(poster);
+    }
+    setPosters(mappedPosters);
+  }
 
   async function getArchive() {
     try {
       const url = "http://localhost:8080/posters/archive";
       const res = await axios.get<IPoster[]>(url);
+      mapPosters(res.data);
       return Promise.resolve(res.data);
     } catch (error) {
       if (axios.isAxiosError(error) && error.response) {
@@ -42,6 +69,7 @@ export default function Archive() {
       }
     };
     fetchAllTags();
+
     const images = () => {
       getArchive().then((data) => {
         setSearchResults(data);
@@ -65,73 +93,34 @@ export default function Archive() {
           <img className="loading-gif" src="/loading.gif" />
         </div>
       )}
-      <main className="happenings">
-        <h1 id="year">2024</h1>
-        <Masonry className="grid" columnsCount={3}>
-          {searchResults.map(
-            (item, index) =>
-              item.startDate[0] === 2024 && (
-                <Box key={index}>
-                  <ImageCard
-                    title={item.title!}
-                    content={item.content!}
-                    startDate={item.startDate!}
-                    endDate={item.endDate!}
-                    location={item.location}
-                    link={item.link}
-                    description={item.description}
-                    tags={item.tags}
-                    recurs={item.isRecurring!}
-                    id={item.id}
-                  />
-                </Box>
-              )
-          )}
-        </Masonry>
-        <h1 id="year">2023</h1>
-        <Masonry className="grid" columnsCount={3}>
-          {searchResults.map(
-            (item, index) =>
-              item.startDate[0] === 2023 && (
-                <Box key={index}>
-                  <ImageCard
-                    title={item.title!}
-                    content={item.content!}
-                    startDate={item.startDate!}
-                    endDate={item.endDate!}
-                    location={item.location}
-                    link={item.link}
-                    description={item.description}
-                    tags={item.tags}
-                    recurs={item.isRecurring!}
-                    id={item.id}
-                  />
-                </Box>
-              )
-          )}
-        </Masonry>
-        <h1 id="year">2022</h1>
-        <Masonry className="grid" columnsCount={3}>
-          {searchResults.map(
-            (item, index) =>
-              item.startDate[0] === 2022 && (
-                <Box key={index}>
-                  <ImageCard
-                    title={item.title!}
-                    content={item.content!}
-                    startDate={item.startDate!}
-                    endDate={item.endDate!}
-                    location={item.location}
-                    link={item.link}
-                    description={item.description}
-                    tags={item.tags}
-                    recurs={item.isRecurring!}
-                    id={item.id}
-                  />
-                </Box>
-              )
-          )}
-        </Masonry>
+      <main className="archive">
+        {Object.keys(posters)
+          .reverse()
+          .map((year, index) => {
+            return (
+              <React.Fragment key={year}>
+                <h1 id="year">{year}</h1>
+                <div className="archive-grid" id={`grid-${year}`}>
+                  {posters[year].map((item, index) => (
+                    <Box key={index}>
+                      <ImageCard
+                        title={item.title!}
+                        content={item.content!}
+                        startDate={item.startDate!}
+                        endDate={item.endDate!}
+                        location={item.location}
+                        link={item.link}
+                        description={item.description}
+                        tags={item.tags}
+                        recurs={item.isRecurring!}
+                        id={item.id}
+                      />
+                    </Box>
+                  ))}
+                </div>
+              </React.Fragment>
+            );
+          })}
         <IconButton
           className="scroll-top"
           color="white"
