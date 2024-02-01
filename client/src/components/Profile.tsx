0d82e4f24746026e0e4b47f5d169f8da95482fa6
Jaclyn Cohen :) <105396093+jackieac04@users.noreply.github.com>
2024-02-01 14:11:34 -0500

    added back anna 's edit profile modal

--- client/src/components/Profile.tsx
+++ client/src/components/Profile.tsx
@@ -11,28 +11,20 @@ import {
   TabPanel,
   Box,
   IconButton,
-  Input,
   Button,
 } from "@chakra-ui/react";
 import Masonry from "masonry-layout";
 import { IPoster } from "./Happenings";
 import imagesLoaded from "imagesloaded";
 import { TriangleUpIcon } from "@chakra-ui/icons";
-import {
-  IUser,
-  classNameTag,
-  createUser,
-  scrollToTop,
-} from "../functions/fetch";
+import { classNameTag, createUser, scrollToTop } from "../functions/fetch";
 import CalendarModal from "./CalendarModal";
 import InterestsModal from "./InterestsModal";
-import axios from "axios";
 import { ProfileImageCard } from "./ProfileImageCard";
 import EditProfileModal from "./EditProfileModal";
 
 export default function Profile() {
-  const [profile, setProfile] = useRecoilState(profileState);
-  const [localProfile, setLocalProfile] = useState<any>({});
+  const [profile] = useRecoilState(profileState);
   const [likeCount, setLikeCount] = useState<number>(0);
   const [createdCount, setCreatedCount] = useState<number>(0);
   const [createdPosters, setCreatedPosters] = useState<IPoster[]>([]);
@@ -134,100 +126,6 @@ export default function Profile() {
     });
   };
 
-  //takes in a file and returns an imgur link
-  const createImgurLink = async (file: File | string) => {
-    const config = {
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
-    };
-
-    try {
-      const url = "http://localhost:8080/posters/uploadToImgur";
-
-      const formData = new FormData();
-      console.log(file);
-      formData.append("content", file);
-      const res = await axios.post(url, formData, config);
-
-      return Promise.resolve(res.data.data);
-    } catch (error) {
-      setIsLoading(false);
-      if (axios.isAxiosError(error) && error.response) {
-        console.log(error.response.data.message);
-        console.log(error);
-        return Promise.resolve(
-          `Error in fetch: ${error.response.data.message}`
-        );
-      } else {
-        return Promise.resolve("Error in fetch: Network error or other issue");
-      }
-    }
-  };
-
-  const handleProfilePictureUpload = async (
-    target: EventTarget & HTMLInputElement
-  ) => {
-    if (target.files) {
-      const file = target.files[0]; //getting the file object
-      const output = await createImgurLink(file);
-      setProfile({ ...profile, picture: output });
-    }
-  };
-
-  const updateValue = (property, value) => {
-    if (value && typeof value === "string") {
-      setProfile({ ...profile, [property]: value });
-    } else if (value instanceof Set) {
-      setProfile({ [property]: Array.from(value) });
-    }
-  };
-
-  async function updateProfile(): Promise<void> {
-    //update profile in database put req
-    try {
-      const updatedUser: IUser = {
-        id: profile.id,
-        name: profile.name,
-        email: profile.email,
-        picture: profile.picture,
-        interests: profile.interests,
-        createdPosters: createdPosters,
-        savedPosters: savedPosters,
-      };
-
-      // console.log("inside createUser", updatedUser);
-      //add to database
-      const config = {
-        headers: {
-          "Content-Type": "application/json",
-        },
-        withCredentials: true,
-      };
-      const url = "http://localhost:8080/users/update/" + profile.id;
-
-      const res = await axios.put(url, updatedUser, config);
-
-      // Set the user profile in state
-      setProfile(updatedUser);
-      setLocalProfile({});
-      localStorage.setItem("userProfile", JSON.stringify(updatedUser));
-
-      setEditingMode(false);
-      return Promise.resolve(res.data.data);
-    } catch (error) {
-      if (axios.isAxiosError(error) && error.response) {
-        console.error(
-          Promise.resolve(`Error in fetch: ${error.response.data.message}`)
-        );
-      } else {
-        console.error(
-          Promise.resolve("Error in fetch: Network error or other issue")
-        );
-      }
-    }
-  }
-
   return (
     <main className="user-page">
       {isLoading && (
@@ -237,185 +135,59 @@ export default function Profile() {
       )}
       {calOpen && <CalendarModal onClose={() => setCalOpen(false)} />}
       <div className="profile">
-        {editingMode ? (
+        {editingMode && (
+          <EditProfileModal
+            savedPosters={savedPosters}
+            createdPosters={createdPosters}
+            onClose={() => setEditingMode(false)}
+          />
+        )}
+        {profile && (
           <>
-            {/* <EditProfileModal onClose={() => setEditingMode(false)} /> */}
-            <div
-              className="pfp-pic"
-              style={{
-                width: "40%",
-                marginTop: "10%",
-              }}
-            >
-              <label
-                htmlFor="profile-upload"
-                className="upload-pfp"
-                style={{
-                  width: "40%",
-                  position: "absolute",
-                  height: "24.5%",
-                  backgroundColor: "gray",
-                  opacity: ".85",
-                  justifyContent: "space-around",
-                  display: "inline-flex",
-                  alignItems: "center",
-                  color: "white",
+            <img className="profile-picture" src={profile.picture} alt="" />
+            <h1 className="name" style={{ marginTop: "1vh" }}>
+              {profile.name}
+            </h1>
+            <p>{profile.email}</p>
+            <div className="icons">
+              <Button
+                className="edit-button"
+                onClick={() => {
+                  setEditingMode(true);
                 }}
               >
-                <p>Upload</p>
-              </label>
-              <Input
-                type="file"
-                onChange={(ev) => handleProfilePictureUpload(ev.target)}
-                id="profile-upload"
-                accept="image/png, image/jpeg, image/jpg"
-                display="none"
-                style={{
-                  width: "100%",
-                  position: "absolute",
-                  borderRadius: "50% !important",
-                }}
-              ></Input>
-              <img
-                src={profile.picture}
-                alt=""
-                style={{ width: "100%", borderRadius: "50%" }}
-              />
-            </div>
-
-            <Input
-              placeholder={profile.name}
-              value={profile.name}
-              onChange={(ev) => updateValue("name", ev.target.value)}
-              style={{ margin: "5%" }}
-            ></Input>
-
-            <p>{profile.email}</p>
-            <div
-              className="icons"
-              style={{
-                alignContent: "center",
-                width: "20%",
-                display: "flex",
-                justifyContent: "space-around",
-              }}
-            >
-              <img
-                style={{
-                  color: "white",
-                  backgroundColor: "white",
-                  width: "60%",
-                }}
-                onClick={updateProfile}
-                src="/check.svg"
-                alt=""
-              />
-              {/* add x that calls setProfile(localProfile) and sets editing mode to false */}
-              <img
-                style={{
-                  color: "white",
-                  backgroundColor: "white",
-                  width: "60%",
-                }}
+                Edit Profile
+              </Button>
+              <Button
+                className="calendar-button"
                 onClick={() => setCalOpen(true)}
-                src="/calendar-day-svgrepo-com.svg"
-                alt=""
-              />
+              >
+                <img
+                  className="calendar-icon"
+                  src="/calendar-day-svgrepo-com.svg"
+                  alt=""
+                />
+              </Button>
             </div>
-            <div
-              className="view-info"
-              style={{
-                fontFamily: "'quicksand', sans-serif",
-                width: "90%",
-                marginLeft: "1.5vw",
-                marginTop: "0vw",
-              }}
-            >
-              <div className="info-rows">
-                <div className="field-name" style={{ width: "20%" }}>
-                  Likes
+            <div className="info-rows">
+              <div className="field-name">Likes</div>
+              <div id="field-data">{likeCount}</div>
+            </div>
+            <div className="info-rows">
+              <div className="field-name">Created</div>
+              <div id="field-data">{createdCount}</div>
+            </div>
+            <div className="info-rows">
+              <div className="field-name">Interests</div>
+            </div>
+            <div id="profile-interests">
+              {Array.from(interests).map((interest, index) => (
+                <div className={classNameTag(index)} key={index}>
+                  {interest}
                 </div>
-                <div id="field-data">{likeCount}</div>
-              </div>
-              <div className="info-rows">
-                <div className="field-name" style={{ width: "30%" }}>
-                  Created
-                </div>
-                <div id="field-data">{createdCount}</div>
-              </div>
-              <div className="info-rows">
-                <div className="field-name" style={{ width: "35%" }}>
-                  Interests
-                </div>
-                <div
-                  id="field-data"
-                  style={{ display: "flex", justifyContent: "end" }}
-                >
-                  <img
-                    style={{ width: "15%" }}
-                    src="/edit.svg"
-                    onClick={() => setInterestsState(true)}
-                  />
-                </div>
-              </div>
-              <div id="field-data">
-                {Array.from(interests).map((interest, indx) => (
-                  <div className={classNameTag(indx)} key={indx}>
-                    {interest}
-                  </div>
-                ))}
-              </div>
+              ))}
             </div>
           </>
-        ) : (
-          profile && (
-            <>
-              <img className="profile-picture" src={profile.picture} alt="" />
-              <h1 className="name" style={{ marginTop: "1vh" }}>
-                {profile.name}
-              </h1>
-              <p>{profile.email}</p>
-              <div className="icons">
-                <Button
-                  className="edit-button"
-                  onClick={() => {
-                    setEditingMode(true);
-                    setLocalProfile(profile);
-                  }}
-                >
-                  Edit Profile
-                </Button>
-                <Button
-                  className="calendar-button"
-                  onClick={() => setCalOpen(true)}
-                >
-                  <img
-                    className="calendar-icon"
-                    src="/calendar-day-svgrepo-com.svg"
-                    alt=""
-                  />
-                </Button>
-              </div>
-              <div className="info-rows">
-                <div className="field-name">Likes</div>
-                <div id="field-data">{likeCount}</div>
-              </div>
-              <div className="info-rows">
-                <div className="field-name">Created</div>
-                <div id="field-data">{createdCount}</div>
-              </div>
-              <div className="info-rows">
-                <div className="field-name">Interests</div>
-              </div>
-              <div id="profile-interests">
-                {Array.from(interests).map((interest, index) => (
-                  <div className={classNameTag(index)} key={index}>
-                    {interest}
-                  </div>
-                ))}
-              </div>
-            </>
-          )
         )}
       </div>
       {interestsState && (
@@ -473,7 +245,6 @@ export default function Profile() {
                     tags={poster.tags}
                     recurs={poster.isRecurring!}
                     id={poster.id}
-                    created={false}
                   />
                 </Box>
               ))}
@@ -494,7 +265,6 @@ export default function Profile() {
                     tags={poster.tags}
                     recurs={poster.isRecurring!}
                     id={poster.id}
-                    created={true}
                   />
                 </Box>
               ))}
