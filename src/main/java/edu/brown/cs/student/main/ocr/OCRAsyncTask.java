98ddb414cd561253885bc43ce6eb8abf7e15b34d
kli4 <katie_li@outlook.com>
2024-06-16 12:36:21 -0700

    call API; TODO: parse results

--- src/main/java/edu/brown/cs/student/main/ocr/OCRAsyncTask.java
+++ src/main/java/edu/brown/cs/student/main/ocr/OCRAsyncTask.java
@@ -1,19 +1,26 @@
 package edu.brown.cs.student.main.ocr;
 
-public class OCRAsyncTask {
-  //  @Autowired private CloudVisionTemplate cloudVisionTemplate;
-  //
-  //  @Autowired private ResourceLoader resourceLoader;
+import java.util.HashMap;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gcp.vision.CloudVisionTemplate;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Service;
 
-  //  public HashMap sendPost(String imageUrl) throws Exception {
-  //
-  //    OCRParser parser = new OCRParser();
-  //    String textFromImage =
-  //
-  // this.cloudVisionTemplate.extractTextFromImage(this.resourceLoader.getResource(imageUrl));
-  //
-  //    System.out.println("Result: " + ("Text from image: " + textFromImage));
-  //
-  //    return parser.deserialize(String.valueOf(textFromImage));
-  //  }
+@Service
+public class OCRAsyncTask {
+  @Autowired private CloudVisionTemplate cloudVisionTemplate;
+
+  @Autowired private ResourceLoader resourceLoader;
+
+  public HashMap sendPost(String imageUrl) throws Exception {
+    System.out.println("start of send post");
+
+    OCRParser parser = new OCRParser();
+    String textFromImage =
+        this.cloudVisionTemplate.extractTextFromImage(this.resourceLoader.getResource(imageUrl));
+
+    System.out.println("Result: " + ("Text from image: " + textFromImage));
+
+    return new HashMap();
+  }
 }
